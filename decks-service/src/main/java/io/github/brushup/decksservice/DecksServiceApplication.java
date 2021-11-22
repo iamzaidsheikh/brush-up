b1286c7495ee3a02f251048892816b50db28e7fe
Zaid <iamzaidsheikh@gmail.com>
2021-11-22 17:26:15 +0530

    build: Set up db connection

--- decks-service/src/main/java/io/github/brushup/decksservice/DecksServiceApplication.java
+++ decks-service/src/main/java/io/github/brushup/decksservice/DecksServiceApplication.java
@@ -1,13 +1,28 @@
 package io.github.brushup.decksservice;
 
+import java.nio.file.Path;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import io.github.brushup.decksservice.connection.DataStaxAstraProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(DataStaxAstraProperties.class)
 public class DecksServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DecksServiceApplication.class, args);
 	}
+
+	@Bean
+    public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties astraProperties) {
+        Path bundle = astraProperties.getSecureConnectBundle().toPath();
+        return builder -> builder.withCloudSecureConnectBundle(bundle);
+    }
+		
 
 }
