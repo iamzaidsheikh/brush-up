5db0dcb03fea6e870fd0f97ef660a7b20865775a
Zaid <iamzaidsheikh@gmail.com>
2021-11-22 16:42:28 +0530

    config: Set up decks service

---
+++ decks-service/src/main/java/io/github/brushup/decksservice/DecksServiceApplication.java
@@ -0,0 +1,13 @@
+package io.github.brushup.decksservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecksServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecksServiceApplication.class, args);
+	}
+
+}
---
+++ decks-service/src/test/java/io/github/brushup/decksservice/DecksServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.brushup.decksservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecksServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
