85ae8025d98ebe9b991eebd29fa716b7ed62494d
Zaid <iamzaidsheikh@gmail.com>
2021-11-23 18:15:10 +0530

    build: Add DeckMapper and create tables

---
+++ decks-service/src/main/java/io/github/brushup/decksservice/DeckMapper.java
@@ -0,0 +1,25 @@
+package io.github.brushup.decksservice;
+
+import com.datastax.oss.driver.api.core.CqlSession;
+import com.datastax.oss.driver.api.mapper.MapperBuilder;
+import com.datastax.oss.driver.api.mapper.annotations.DaoFactory;
+import com.datastax.oss.driver.api.mapper.annotations.Mapper;
+
+import io.github.brushup.decksservice.dao.CardDao;
+import io.github.brushup.decksservice.dao.DeckDao;
+
+@Mapper
+public interface DeckMapper {
+
+    @DaoFactory
+    DeckDao deckDao();
+
+    @DaoFactory
+    CardDao cardDao();
+    
+    static MapperBuilder<DeckMapper> builder(CqlSession session) {
+        return new DeckMapperBuilder(session);
+    }
+    
+}
+
--- decks-service/src/main/java/io/github/brushup/decksservice/DecksServiceApplication.java
+++ decks-service/src/main/java/io/github/brushup/decksservice/DecksServiceApplication.java
@@ -1,5 +1,7 @@
 package io.github.brushup.decksservice;
 
+import java.io.FileNotFoundException;
+
 import com.datastax.oss.driver.api.core.CqlSession;
 
 import org.springframework.boot.SpringApplication;
@@ -8,6 +10,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 
 import io.github.brushup.decksservice.connection.DataStaxAstraProperties;
+import io.github.brushup.decksservice.utils.CqlFileUtils;
 
 @SpringBootApplication
 @EnableConfigurationProperties(DataStaxAstraProperties.class)
@@ -27,7 +30,45 @@ public class DecksServiceApplication {
 				.withKeyspace(astraProperties.getKeyspace())
 				.build();
 
+		createSchemaIfNeeded(cqlSession);		
+
 		return cqlSession;
 	}
 
+	public void createSchemaIfNeeded(CqlSession cqlSession) {
+        try {
+            CqlFileUtils.executeCQLFile(cqlSession, "schema.cql");
+        } catch (FileNotFoundException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+	@Bean
+	public DeckMapper getMapper(CqlSession session) {
+		return new DeckMapperBuilder(session).build();
+	}
+
+	// @Bean
+	// CommandLineRunner run(DeckMapper deckMapper) {
+	// 	return args -> {
+			
+	// 		Card card = new Card();
+	// 		card.setId(UUID.randomUUID());
+	// 		card.setFront("This text is in front");
+	// 		card.setBack("This text in the back");
+
+	// 		CardDao cardDao = deckMapper.cardDao();
+	// 		cardDao.save(card);
+
+	// 		Deck deck = new Deck();
+	// 		deck.setId(UUID.randomUUID());
+	// 		deck.setNumCards(1);
+	// 		List<Card> cards = new ArrayList<>();
+	// 		cards.add(card);
+	// 		deck.setCards(cards);
+
+	// 		DeckDao deckDao = deckMapper.deckDao();
+	// 		deckDao.save(deck);
+	// 	};
+	// }
 }
