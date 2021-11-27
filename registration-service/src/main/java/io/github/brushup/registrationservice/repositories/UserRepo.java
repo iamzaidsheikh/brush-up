158e38eac5b1b68b072aa21feeb323d96d1fd87c
Zaid <iamzaidsheikh@gmail.com>
2021-11-27 18:04:22 +0530

    build: Update User and Role repo

--- registration-service/src/main/java/io/github/brushup/registrationservice/repositories/UserRepo.java
+++ registration-service/src/main/java/io/github/brushup/registrationservice/repositories/UserRepo.java
@@ -1,12 +1,10 @@
 package io.github.brushup.registrationservice.repositories;
 
-import java.util.UUID;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import io.github.brushup.registrationservice.models.User;
 
-public interface UserRepo extends JpaRepository<User, UUID>{
+public interface UserRepo extends JpaRepository<User, Long>{
     
     User findByUsername(String username);
     
